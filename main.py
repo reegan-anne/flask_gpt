7aef02436ab25e1544047f277c4eea8cf1497314
reegan-anne <reegzcaine@yahoo.com>
2023-01-30 14:37:06 +0200

    Added for loop method

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'Welcome %s' % name
+@app.route('/')
+def student():
+   return render_template('student.html')
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
    if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug = True)
